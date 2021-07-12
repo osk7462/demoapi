887562ba21266d7db949b3815b45dc955c6e3a07
osama khan <osk7462@gmail.com>
2021-07-12 11:16:46 +0545

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,10 +138,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST= [
-    # "http://localhost:3000",
-    "https://osk7462.netlify.app/",
-]
+# CORS_ORIGIN_WHITELIST= [
+#     # "http://localhost:3000",
+#     "https://osk7462.netlify.app/",
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
